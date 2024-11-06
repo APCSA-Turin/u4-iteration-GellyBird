975b4d13fb7d4dc1ff15f81d4051153b697e520e
GellyBird <147560865+GellyBird@users.noreply.github.com>
2024-11-06 16:06:12 +0000

    sync or something

---
+++ U4T1_Lab1.java
@@ -0,0 +1,5 @@
+public class U4T1_Lab1{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ U4T2_ForLoops/U4T2_Lab1.java
@@ -0,0 +1,8 @@
+public class U4T2_Lab1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
